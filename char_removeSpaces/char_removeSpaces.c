c0cf0b43b4708fcbaa2de1419b33d6f0b4c479ec
TalMekler <tal.mekler11@gmail.com>
2022-02-25 13:10:58 +0200

    remove extra spaces from string

---
+++ char_removeSpaces/char_removeSpaces.c
@@ -0,0 +1,22 @@
+void removeSpaces(char* st) {
+	for (int i = 0; i < strlen(st); ++i) {
+		if (i == 0 && st[i] == ' ') {
+			for (int j = i; j < strlen(st); ++j) {
+				st[j] = st[j + 1];
+			}
+			--i;
+		}
+
+		if (i > 0) {
+			if (st[i - 1] == ' ' && st[i] == ' ') {
+				for (int j = i; j < strlen(st); ++j) {
+					st[j] = st[j + 1];
+				}
+				--i;
+			}
+		}
+	}
+
+	if (st[strlen(st) - 1] == ' ')
+		st[strlen(st) - 1] = NULL;
+}
---
+++ char_removeSpaces/char_removeSpaces.h
@@ -0,0 +1,1 @@
+void removeSpaces(char* st);
