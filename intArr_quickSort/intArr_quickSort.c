3999c180914bcc900484300d4fab03ec543df399
TalMekler <tal.mekler11@gmail.com>
2022-03-08 20:54:03 +0200

    Update intArr_quickSort.c

--- intArr_quickSort/intArr_quickSort.c
+++ intArr_quickSort/intArr_quickSort.c
@@ -1,41 +1,31 @@
+// *** important: At the first call: h = the last INDEX in the array (size-1)
+void quickSort(int *arr, int l, int h){
+    int j;
+    if(l<h){
+        j = quickSort_swapper(arr, l, h);
+        quickSort(arr, l, j); 
+        quickSort(arr, j+1, h);
+    }
+}
 
-int partition(int* arr, int low, int high) {
-    int pivot = arr[low];
-    int i = low, j = high, tmp;
-    while (i < j) {
-
-        do {
-            ++i;
-        } while (arr[i] <= pivot && i < high);
-
-        do {
-            --j;
-        } while (arr[j] > pivot && j > low);
-
-        if (i < j) {
+int quickSort_swapper(int *arr, int l, int h){
+    int pivot = arr[l];
+    int tmp ,i = l ,j = h;
+    
+    while(i<j) {
+        for (; i<h && arr[i] <= pivot; i++); // skip i to the index where the value is BIGGER then the pivot value
+        for (; j>l && arr[j] > pivot; j--); // skip j to the index where the value is SMALLER then the pivot value
+        
+        // swap values if i < j
+        if(i<j) {
             tmp = arr[i];
             arr[i] = arr[j];
             arr[j] = tmp;
         }
-
     }
-    tmp = arr[low];
-    arr[low] = arr[j];
+    // i > j -> swap j value with pivot value
+    tmp = arr[l];
+    arr[l] = arr[j];
     arr[j] = tmp;
-
-    return j;
-}
-
-void quickSort(int* arr, int l, int h) {
-    int j;
-    static cnt = 0;
-    if (l < h) {
-        ++cnt;
-        j = partition(arr, l, h);
-        quickSort(arr, l, j);
-        quickSort(arr, j + 1, h);
-    }
-    else {
-        printf("quickSort cnt: %d", cnt);
-    }
+    return j; // pivot value in the correct position in the array - return the position
 }
